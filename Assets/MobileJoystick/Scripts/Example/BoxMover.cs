de509f1b3f1a1d0c6474522c4e48e639c5805788
DariaTiessova <bermuds@inbox.ru>
2021-09-15 17:41:09 +0600

    first commit, управление игроком, движение игрока

---
+++ Assets/MobileJoystick/Scripts/Example/BoxMover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using DitzeGames.MobileJoystick;
+
+namespace DitzeGames.MobileJoystick.Example
+{
+    public class BoxMover : MonoBehaviour {
+
+    protected FixedJoystick Joystick;
+    protected Button Button;
+    protected TouchField TouchField;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Joystick = FindObjectOfType<FixedJoystick>();
+        Button = FindObjectOfType<Button>();
+        TouchField = FindObjectOfType<TouchField>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(transform.position.x + Joystick.inputVector.x * Time.deltaTime * 3f, Button.Pressed ? 2 : 1, transform.position.z + Joystick.inputVector.y * Time.deltaTime * 3f);
+        transform.Rotate(Vector3.up, TouchField.TouchDist.x);
+        transform.Rotate(Vector3.left, TouchField.TouchDist.y);
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public FixedJoystick Joystick;
+ 
+    public float speed;
+    private bool isTurnRight = true;
+    private bool isTurnUp = true;
+    public Sprite spriteForward;
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+    public SpriteRenderer spriteRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+       
+
+        float moveForward = Joystick.inputVector.x;
+        float moveUp = Joystick.inputVector.y;
+        Debug.Log(moveUp);
+        if (moveForward != 0)
+        {
+            spriteRenderer.sprite = spriteForward;
+            transform.Translate(Vector3.right * moveForward * speed * Time.deltaTime);
+            if (moveForward > 0 && !isTurnRight)
+            {
+                TurnRight();
+
+            }
+            else if (moveForward < 0 && isTurnRight)
+            {
+
+                TurnRight();
+
+            }
+        }
+       if (moveUp >= 0.9f || moveUp <= -0.9f)
+        {
+           // spriteRenderer.sprite = spriteUp;
+            transform.Translate(Vector3.up * moveUp * speed * Time.deltaTime);
+            if (moveUp > 0 )
+            {
+                spriteRenderer.sprite = spriteUp;
+
+            }
+            else if (moveUp < 0 )
+            {
+
+                spriteRenderer.sprite = spriteDown;
+
+            }
+        }
+       
+    }
+
+    void TurnRight()
+    {
+        isTurnRight = !isTurnRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    } 
+    
+    void TurnUp()
+    {
+        isTurnUp = !isTurnUp;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.y *= -1;
+        transform.localScale = theScale;
+    }
+}
